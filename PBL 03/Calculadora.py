364285085c72d903c4e211a86b73e41aa6e0e57b
Bernardo <55258876+Berdegeus@users.noreply.github.com>
2023-03-31 11:16:24 -0300

    add the loop

--- PBL 03/Calculadora.py
+++ PBL 03/Calculadora.py
@@ -1,40 +1,44 @@
-##calculator
+def soma(x, y):
+    return x + y
 
-def soma(x,y):
-    return x+y
+def subtracao(x, y):
+    return x - y
 
-def subtracao(x,y):
-    return x-y
+def multiplicacao(x, y):
+    return x * y
 
-def multiplicacao(x,y):
-    return x*y
-
-def divisao(x,y):
-    return x/y
+def divisao(x, y):
+    return x / y
 
 
 def main():
-    print("Escolha a operação:")
-    print("1 - Soma")
-    print("2 - Subtração")
-    print("3 - Multiplicação")
-    print("4 - Divisão")
+    while True:
+        print("Escolha a operação:")
+        print("1 - Soma")
+        print("2 - Subtração")
+        print("3 - Multiplicação")
+        print("4 - Divisão")
+        print("5 - Sair")
 
-    escolha = input('Digite sua escolha (1/2/3/4):')
+        escolha = input("Digite sua escolha (1/2/3/4/5): ")
+        if escolha == '5':
+            print("Até mais!")
+            break
+        elif escolha not in ('1', '2', '3', '4'):
+            print("Opção inválida")
+            continue
 
-    num1 = int(input('Digite o primeiro número: '))
-    num2 = int(input('Digite o segundo número: '))
-    if escolha == '1':
-        print(num1, '+', num2, '=', soma(num1,num2))
-    elif escolha == '2':
-        print(num1, '-', num2, '=', subtracao(num1,num2))
-    elif escolha == '3':
-        print(num1, '*', num2, '=', multiplicacao(num1,num2))
-    elif escolha == '4':
-        print(num1, '/', num2, '=', divisao(num1,num2))
-    else:
-        print('Opção inválida')
+        num1 = int(input('Digite o primeiro número: '))
+        num2 = int(input('Digite o segundo número: '))
+
+        if escolha == '1':
+            print(num1, '+', num2, '=', soma(num1, num2))
+        elif escolha == '2':
+            print(num1, '-', num2, '=', subtracao(num1, num2))
+        elif escolha == '3':
+            print(num1, '*', num2, '=', multiplicacao(num1, num2))
+        elif escolha == '4':
+            print(num1, '/', num2, '=', divisao(num1, num2))
 
 if __name__ == '__main__':
     main()
-
