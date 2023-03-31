9e48a5e28416d141c2f3bb58064c996a002b5d4d
Bernardo <55258876+Berdegeus@users.noreply.github.com>
2023-03-31 10:39:06 -0300

    Obrigado stephano, rei do python, por me ajudar.

--- aula 5/4.py
+++ aula 5/4.py
@@ -1,7 +1,10 @@
-## input a N number and print all the numbers from 1 to N
+## input a N number and sum all the numbers from 1 to N
 
 n = int(input('digite um numero: '))
+sum = 0
 
-for i in range(1, n + 1):
-    print(i)
+for i in range(1, n+1):
+    sum += i
+print(f('a soma de todos os numeros de 1 ate {n} e {sum}'))
+
 
