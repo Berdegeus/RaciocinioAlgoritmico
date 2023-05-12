775ec5184cbe4f8fa29435a3a9ed629560677b9b
Bernardo <55258876+Berdegeus@users.noreply.github.com>
2023-05-12 11:26:22 -0300

    Update 10.py

--- PBL 05/10.py
+++ PBL 05/10.py
@@ -1,8 +1,18 @@
-vetor = [2, 4, 7, 2, 3, 3, 1, 0, 2, 6]
+number = [2, 4, 7, 2, 3, 1, 0, 2, 6]
+count = {}
 
-for i in range(len(vetor)):
-    if vetor.count(vetor[i]) > 1:
-        print(vetor[i])
-        break
+for n in number:
+    if n in count:
+        count[n] += 1
     else:
-        continue
+        count[n] = 1
+
+most_common_number = None
+max_count = 0
+
+for n, c in count.items():
+    if c > max_count:
+        most_common_number = n
+        max_count = c
+
+print(f"O número que aparece com mais frequência é: {most_common_number}")
